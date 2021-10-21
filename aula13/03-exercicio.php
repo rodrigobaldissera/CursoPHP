3b8b3f13c264d2b072a3d2003ae5db9b6f1ffaf7
rodrigobaldissera <rodrigo.baldissera@pucpr.edu.br>
2021-10-20 22:03:52 -0300

    update

---
+++ aula13/03-exercicio.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <link rel="stylesheet" href="_css/estilo.css"/>
+  <meta charset="UTF-8"/>
+  <title>Curso de PHP - CursoemVideo.com</title>
+</head>
+<body>
+<div>
+    <?php
+        $num = isset($_GET["num"])?$_GET["num"]:1;
+        echo "<h2>Analisando o número $num ...</h2><br>";
+        for ($cont=$num)
+
+    ?>
+</div>
+</body>
+</html>
+ 
