ce694b5cc2e7507892407ce45d094be0585d7658
rodrigobaldissera <rodrigo.baldissera@pucpr.edu.br>
2021-11-02 19:17:18 -0300

    update

---
+++ aula17/23-str_pad.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <link rel="stylesheet" href="_css/estilo.css"/>
+  <meta charset="UTF-8"/>
+  <title>Curso de PHP - CursoemVideo.com</title>
+</head>
+<body>
+<div>
+    <?php
+      $nome = "Guanabara";
+      $novo = str_pad($nome, 30, " ", STR_PAD_RIGHT);
+      print("Meu professor $novo é legal!");
+    ?>
+</div>
+</body>
+</html>
+ 
